ba606321708d33180df24c7d0cbb270f7d799bfc
Isabelle Garcia <isabellegarcia@Isabelles-MBP.fios-router.home>
2015-12-28 20:06:28 -0500

    added template parts and more styles for sticky post

---
+++ part-main-top.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Include the top widgets
+ *
+ * @package WordPress
+ * @subpackage PostPress 1.0
+ * @since PostPress 1.0
+ */
+?>
+          <?php if ( is_active_sidebar( 'top-widgets' )  ) : ?>
+            <div id="top-widgets"><?php get_sidebar('top'); ?></div>
+          <?php endif; ?> 
