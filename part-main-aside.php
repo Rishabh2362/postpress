0f369a70a287a46f0ad84ece28d65f24c50fd6b6
Isabelle Garcia <garciaisabelle@icloud.com>
2016-10-25 09:39:25 +0200

    added a category page, a tag page (Although it did not work, need to fix this) and unified the indentations

--- part-main-aside.php
+++ part-main-aside.php
@@ -1,47 +1,47 @@
 <?php
 /**
- * Include PostPress Slider 
- *
- * @package PostPress
- * @subpackage PostPress 1.0
- * @since PostPress 1.0
- */
+* Include PostPress Slider 
+*
+* @package PostPress
+* @subpackage PostPress 1.0
+* @since PostPress 1.0
+*/
 ?>
 <?php
-            if (get_theme_mod('icon1_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon1_link')); ?>" target="_blank"><i class="fa fa-facebook-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon2_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon2_link')); ?>" target="_blank"><i class="fa fa-twitter-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon3_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon3_link')); ?>" target="_blank"><i class="fa fa-google-plus-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon4_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon4_link')); ?>" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon5_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon5_link')); ?>" target="_blank"><i class="fa fa-linkedin-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon6_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon6_link')); ?>" target="_blank"><i class="fa fa-youtube-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon7_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon7_link')); ?>" target="_blank"><i class="fa fa-pinterest-square fa-lg"></i></a>
-            <?php } 
-            if (get_theme_mod('icon8_link')) { ?>
-            <a href="<?php echo esc_url(get_theme_mod('icon8_link')); ?>" target="_blank"><i class="fa fa-tumblr-square fa-lg"></i></a>
-            <?php } ?>
+if ( get_theme_mod( 'icon1_link') ) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon1_link')); ?>" target="_blank"><i class="fa fa-facebook-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon2_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon2_link')); ?>" target="_blank"><i class="fa fa-twitter-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon3_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon3_link')); ?>" target="_blank"><i class="fa fa-google-plus-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon4_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon4_link')); ?>" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon5_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon5_link')); ?>" target="_blank"><i class="fa fa-linkedin-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon6_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon6_link')); ?>" target="_blank"><i class="fa fa-youtube-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon7_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon7_link')); ?>" target="_blank"><i class="fa fa-pinterest-square fa-lg"></i></a>
+<?php } 
+if (get_theme_mod('icon8_link')) { ?>
+<a href="<?php echo esc_url(get_theme_mod('icon8_link')); ?>" target="_blank"><i class="fa fa-tumblr-square fa-lg"></i></a>
+<?php } ?>
 
-            <?php if ( is_active_sidebar( 'primary-sidebar' )  ) : ?>
-            <div class="hidden-lg-up">
-              <br>
-              <?php get_sidebar('primary'); ?>
-            </div>
-            <?php endif; ?> 
-            <?php if ( is_active_sidebar( 'secondary-sidebar' )  ) : ?>
-            <div>
-              <br>
-              <?php get_sidebar('secondary'); ?>
-            </div>
-            <?php endif; ?> 
+<?php if ( is_active_sidebar( 'primary-sidebar' )  ) : ?>
+<div class="hidden-lg-up">
+  <br>
+  <?php get_sidebar('primary'); ?>
+</div>
+<?php endif; ?> 
+<?php if ( is_active_sidebar( 'secondary-sidebar' )  ) : ?>
+<div>
+  <br>
+  <?php get_sidebar('secondary'); ?>
+</div>
+<?php endif; ?> 
--- searchform.php
+++ searchform.php
@@ -9,9 +9,9 @@
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group">
-      <input type="text" class="form-control" placeholder="Search for..." name="s" id="s" >
-      <span class="input-group-btn">
-        <button class="btn btn-secondary" type="button">Go!</button>
-      </span>
-    </div>
+	  <input type="text" class="form-control" placeholder="Search for..." name="s" id="s" >
+	  <span class="input-group-btn">
+		<button class="btn btn-secondary" type="button">Go!</button>
+	  </span>
+	</div>
 </form>
