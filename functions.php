b273f355acfede362b24e1c5a76fd6f608eeb3b3
Isabelle Garcia <garciaisabelle@icloud.com>
2016-10-26 13:44:22 +0200

    fixed the tag issue with the design getting broke. It was because Bootstrap has a class named .tag wich conflicts with the WP body_class() .tag. I added a fliter in the function file to rm that class. I hope this will be ok with the theme review though...

--- functions.php
+++ functions.php
@@ -38,6 +38,18 @@ function postpress_setup() {
 	) );
 }
 
+/*
+ * Removes the class "tag" from the body_class array.
+ * Because it conflicts with Bootstrap 4 .tag class.
+ */ 
+add_filter( 'body_class', 'adjust_body_class' );
+function adjust_body_class( $classes ) {
+	foreach ( $classes as $key => $value ) {
+		if ( $value == 'tag' ) unset( $classes[ $key ] );
+	}
+	return $classes;
+}
+
 /**
  * Register widget areas.
  */
