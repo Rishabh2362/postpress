0de761080167e27d1f32f22a348ef9c012085478
Isabelle Garcia <garciaisabelle@icloud.com>
2016-11-17 22:11:15 +0100

    added the credits in the primary sidebar

--- sidebar.php
+++ sidebar.php
@@ -11,4 +11,13 @@
 ?>
 <aside>
 	<?php dynamic_sidebar( 'primary-sidebar' ); ?>
+	<small id="credits">
+		<?php
+		$url1 = 'http://www.isabellegarcia.me';
+		$url2 = 'http://www.aicragellebasi.social';
+		$link = sprintf( wp_kses( __( '<a href="%1$s" target="_blank">PostPress</a>, a WordPress Theme by <a href="%2$s" target="_blank">@aicragellebasi</a>', 'iamsocial' ), array(  'a' => array( 'href' => array() ) ) ), esc_url( $url1 ), esc_url( $url2 ) );
+		echo $link;
+		?>
+		</em>
+	</small>
 </aside>
