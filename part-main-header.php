ab2e05da10d1d262c3d42792b6ea03120f9b5d82
Isabelle Garcia <garciaisabelle@icloud.com>
2016-12-10 13:06:46 -0500

    custom header implemented and styled with the Bootstrap card class Image overlays

--- part-main-header.php
+++ part-main-header.php
@@ -8,6 +8,11 @@
 */
 ?>
 <?php if ( get_header_image() ) : ?>
-<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" class="img-responsive" />
+<div class="card card-inverse">
+  <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" class="img-responsive" />
+  <div class="card-img-overlay">
+    <h4 class="card-title"><?php bloginfo( 'name' ); ?></h4>
+    <p class="card-text"><?php bloginfo( 'description' ); ?></p>
+  </div>
+</div>
 <?php endif; ?>
-
