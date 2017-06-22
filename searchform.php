3e28a4f00d00a78cc606b3a890e24e564ea3284a
Isabelle Garcia <garciaisabelle@icloud.com>
2017-06-22 13:21:39 +0200

    REQUIRED: Use &hellip; instead of ...

--- searchform.php
+++ searchform.php
@@ -10,7 +10,7 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group">
 	  <label class="sr-only" for="s"><?php esc_html_e( 'Search for&hellip;', 'postpress' ) ?></label>
-	  <input type="text" class="form-control" placeholder="<?php esc_attr_e( 'Search for...', 'postpress' ) ?>" name="s" id="s" value="<?php echo get_search_query(); ?>" >
+	  <input type="text" class="form-control" placeholder="<?php esc_attr_e( 'Search for&hellip;', 'postpress' ) ?>" name="s" id="s" value="<?php echo get_search_query(); ?>" >
 	  <span class="input-group-btn">
 		<button class="btn btn-secondary" type="button"><?php esc_html_e( 'Go!', 'postpress' ) ?></button>
 	  </span>
