8cebb1461b69b367b59387ece954ddfc8bd627fc
Isabelle Garcia <garciaisabelle@icloud.com>
2017-06-22 13:13:06 +0200

    REQUIRED: part-main-header.php - Escape value of height and width with esc_attr().

--- part-main-header.php
+++ part-main-header.php
@@ -10,7 +10,7 @@
 <?php if ( get_header_image() ) : ?>
 	<?php if ( ! ( is_single() || is_page() ) ) { ?>
 		<div class="card card-inverse">
-		  <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" class="img-responsive" />
+		  <img src="<?php header_image(); ?>" height="<?php esc_attr_e( get_custom_header()->height ) ?>" width="<?php esc_attr_e( get_custom_header()->width ) ?>" alt="" class="img-responsive" />
 		  <div class="card-img-overlay">
 		    <p class="card-title display-4"><?php bloginfo( 'name' ); ?></p>
 		    <p class="card-text lead"><?php bloginfo( 'description' ); ?></p>
